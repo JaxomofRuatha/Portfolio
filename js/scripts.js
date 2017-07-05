664d3a39b1c54c517f6c11c089aeaaab86b2b469
John Chritton <jchritton33@gmail.com>
2017-07-05 12:25:45 -0500

    Scrolling working both ways, placeholder logo

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 "use strict";
-
+var pixScrolled = window.innerHeight * 0.4;
+var activeBar = false;
 window.onscroll = function () {
-  if (document.body.scrollTop > 360 || document.documentElement.scrollTop > 360) {
-    document.getElementById("navbar").style.backgroundColor = "black";
+  if (document.body.scrollTop > pixScrolled || document.documentElement.scrollTop > pixScrolled) {
+    document.getElementById("navbar").classList.add("scrolledNav");
+    activeBar = true;
+  }
+  else if (document.body.scrollTop < pixScrolled || document.documentElement.scrollTop < pixScrolled && activeBar == true) {
+    document.getElementById("navbar").classList.remove("scrolledNav");
   }
 }
