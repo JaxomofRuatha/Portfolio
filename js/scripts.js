1256de15d1bb0986e58fef666c73d7d1ba44c148
JaxomofRuatha <jchritton33@gmail.com>
2017-09-11 19:25:02 -0500

    Skill icons animation mostly working

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,38 @@ window.onscroll = function () {
     document.getElementByClassName("navbar").classList.remove("scrolledNav");
   }
 }*/
-const jquery = require('jquery');
+//const jquery = require('jquery');
+
+$(document).ready(() => {
+
+  $(window).scroll(() => {
+    let pixScrolled = window.innerHeight * 0.3;
+    let activeBar = false;
+
+    if (document.body.scrollTop > pixScrolled || document.documentElement.scrollTop > pixScrolled) {
+      $("nav").addClass("scrolledNav")
+      activeBar = true;
+    }
+    else if (document.body.scrollTop < pixScrolled || document.documentElement.scrollTop < pixScrolled && activeBar == true) {
+      $("nav").removeClass("scrolledNav");
+    }
+  })
+
+  $(".skill-icons__buttons button").click((event) => {
+    console.log(event.target.id);
+    if (event.target.id === "web") {
+      $(".skill-icons__display")
+        .html(`
+          <img class="tech-section__icon" src="./images/htmlicon.svg">
+          <img class="tech-section__icon" src="./images/css3icon.svg">
+          <img class="tech-section__icon" src="./images/reacticon.svg">
+          <img class="tech-section__icon" src="./images/jqueryicon.svg">
+          <img class="tech-section__icon" src="./images/bootstrapicon.svg">
+          <img class="tech-section__icon" src="./images/sassicon.svg">
+        `).animate({
+          height: "toggle",
+          opacity: 1
+        }, 1000);
+    }
+  })
+})
