51f1d381060c3204daef440ef7bebd4ef8df9d67
JaxomofRuatha <jchritton33@gmail.com>
2017-10-13 03:47:00 -0500

    Revised Gulp tasks, crude scroll event works

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,34 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+const gutil = require('gulp-util');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
 const pug = require('gulp-pug');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const watchify = require('watchify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const assign = require('lodash.assign');
+
+const customOpts = {
+    entries: ['./js/index.js'],
+    debug: true
+};
+const opts = assign({}, watchify.args, customOpts);
+const bConfig = watchify(browserify(opts));
 
 gulp.task('sass', () => {
-    return gulp.src('./sass/*.scss')
-        .pipe(plumber())    
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
-});
-
-gulp.task('watch', () => {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-    gulp.watch('./view/**/*.pug', ['pug']);
+    return gulp
+      .src('./sass/style.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(plumber())
+      .pipe(autoprefixer({ browsers: ['last 3 versions'] }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./css'));
 });
 
 gulp.task('pug', () => {
@@ -20,4 +36,28 @@ gulp.task('pug', () => {
         .pipe(plumber())     
         .pipe(pug())
         .pipe(gulp.dest('./'))
+});
+
+function bundle() {
+    return bConfig
+      .transform(babelify, { presets: ['env'] })
+      .bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(plumber())
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(uglify())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./js'));
+}
+
+gulp.task('js', bundle);
+bConfig.on('update', bundle);
+bConfig.on('log', gutil.log);
+
+gulp.task('default', ['sass', 'pug', 'js'], () => {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./view/**/*.pug', ['pug']);
+  gulp.watch('./js/**/*.js', ['js']);  
 });
