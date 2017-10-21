5f4d4c88fa142546575c55e9e6a057593799a605
JaxomofRuatha <jchritton33@gmail.com>
2017-10-21 03:05:41 -0500

    Tabs working except for bounce

--- js/index.js
+++ js/index.js
@@ -1,6 +1,70 @@
 //const Scrollbar = require("smooth-scrollbar");
 
 document.addEventListener("DOMContentLoaded", () => {
-    //Attach scroll event listener to container, array for data-value and scroll rotates between
-    
+
+    document.querySelector('.skill-tabs').addEventListener('click', e => {
+        const targetIcons = document.querySelector(e.target.dataset.target);
+        const priorActive = document.querySelector('.tab-active').dataset.target;
+
+        console.log(targetIcons, priorActive);
+
+        document
+            .querySelectorAll('.skill-tabs a')
+            .forEach(a => {
+            a.classList.remove('tab-active');
+            });
+
+        e.target.classList.add('tab-active');
+
+        if (e.target.tagName === 'A' && document.querySelector(`${priorActive}`) !== targetIcons) {
+          document
+            .querySelector(`${priorActive}`)
+              .classList.add('hidden-tile');
+            document
+              .querySelector(`${priorActive}`)
+              .childNodes.forEach(icon => {
+                icon.classList.add('hidden-tile');
+              });  
+          setTimeout(() => {
+              targetIcons.classList.remove('hidden-tile');
+                targetIcons.childNodes.forEach(icon => {
+              icon.classList.remove('hidden-tile');
+            });  
+          }, 500);
+        }
+        /*
+        if (targetIcons.classList.contains('hidden-tile')) {
+            setTimeout(() => {
+            targetIcons.classList.remove('hidden-tile');
+            }, 500);
+        } else {
+            targetIcons.classList.add('hidden-tile');
+        }*/
+    });
+/*
+    document.querySelector('.skill-tabs').addEventListener('click', e => {
+        const targetIcons = document.querySelector(
+            e.target.dataset.target
+        );
+        const priorActive = document.querySelector('.tab-active').dataset.target;
+
+        document.querySelectorAll('.skill-tabs a').forEach(a => {
+            a.classList.remove('tab-active');
+        });
+
+        e.target.classList.add('tab-active');
+
+        document
+          .querySelectorAll(`${priorActive} img`)
+          .forEach(icon => {
+              icon.classList.add('hidden-tile');
+          });
+
+        if (e.target.tagName === 'A') {
+            targetIcons.childNodes.forEach(icon => {
+              icon.classList.remove('hidden-tile');
+            });
+        }
+    });
+    */
 })
