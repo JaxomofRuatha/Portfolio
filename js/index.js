443adbf7cd7996d9aa27ae7db3d451abf523f4d2
JaxomofRuatha <jchritton33@gmail.com>
2017-10-20 19:38:12 -0500

    Debating placement of social navigation

--- js/index.js
+++ js/index.js
@@ -3,19 +3,4 @@
 document.addEventListener("DOMContentLoaded", () => {
     //Attach scroll event listener to container, array for data-value and scroll rotates between
     
-    //Scrollbar.init(document.querySelector('#my-scrollbar'));
-
-    const scrollOrder = ["home", "profile", "projects", "contact"];
-    let currentFrame = 0;
-
-    document.querySelector(".view-container").addEventListener("scroll", (e) => {
-        if (currentFrame >= 0) {
-            //Detect whether scrolling up or down?
-            document.querySelector(`.frame-home`).classList.add('scrolled-past-frame');
-            document.querySelectorAll('.frame-profile').forEach((element) => {
-                console.log(element);
-                element.classList.remove('.pending-frame');
-            });
-        }
-    })
 })
