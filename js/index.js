53fd8ec6909c70e1d6016469f2d8c360e3e9b6a2
JaxomofRuatha <jchritton33@gmail.com>
2017-10-23 01:35:41 -0500

    Adding profile content CSS

--- js/index.js
+++ js/index.js
@@ -11,52 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
         document
             .querySelectorAll('.skill-tabs a')
             .forEach(a => {
-            a.classList.remove('tab-active');
+                a.classList.remove('tab-active');
             });
 
         e.target.classList.add('tab-active');
 
         if (e.target.tagName === 'A' && document.querySelector(`${priorActive}`) !== targetIcons) {
-          document
-            .querySelector(`${priorActive}`)
-              .classList.add('hidden-tile');
-          setTimeout(() => {
-              targetIcons.classList.remove('hidden-tile');  
-          }, 500);
-        }
-        /*
-        if (targetIcons.classList.contains('hidden-tile')) {
+            document
+                .querySelector(`${priorActive}`)
+                .classList.remove('visible-tile');
             setTimeout(() => {
-            targetIcons.classList.remove('hidden-tile');
+                targetIcons.classList.add('visible-tile');
             }, 500);
-        } else {
-            targetIcons.classList.add('hidden-tile');
-        }*/
-    });
-/*
-    document.querySelector('.skill-tabs').addEventListener('click', e => {
-        const targetIcons = document.querySelector(
-            e.target.dataset.target
-        );
-        const priorActive = document.querySelector('.tab-active').dataset.target;
-
-        document.querySelectorAll('.skill-tabs a').forEach(a => {
-            a.classList.remove('tab-active');
-        });
-
-        e.target.classList.add('tab-active');
-
-        document
-          .querySelectorAll(`${priorActive} img`)
-          .forEach(icon => {
-              icon.classList.add('hidden-tile');
-          });
-
-        if (e.target.tagName === 'A') {
-            targetIcons.childNodes.forEach(icon => {
-              icon.classList.remove('hidden-tile');
-            });
         }
     });
-    */
-})
+});
