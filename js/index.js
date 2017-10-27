eaf894967b69eee001a41164187c01eed09b8ca3
JaxomofRuatha <jchritton33@gmail.com>
2017-10-26 20:22:34 -0500

    Adapting projects JS for new layout

--- js/index.js
+++ js/index.js
@@ -1,28 +1,41 @@
 //const Scrollbar = require("smooth-scrollbar");
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.skill-tabs').addEventListener('click', e => {
+    const targetIcons = document.querySelector(e.target.dataset.target);
+    const priorActive = document.querySelector('.tab-active').dataset.target;
 
-    document.querySelector('.skill-tabs').addEventListener('click', e => {
-        const targetIcons = document.querySelector(e.target.dataset.target);
-        const priorActive = document.querySelector('.tab-active').dataset.target;
-
-        console.log(targetIcons, priorActive);
-
-        document
-            .querySelectorAll('.skill-tabs a')
-            .forEach(a => {
-                a.classList.remove('tab-active');
-            });
-
-        e.target.classList.add('tab-active');
-
-        if (e.target.tagName === 'A' && document.querySelector(`${priorActive}`) !== targetIcons) {
-            document
-                .querySelector(`${priorActive}`)
-                .classList.remove('visible-tile');
-            setTimeout(() => {
-                targetIcons.classList.add('visible-tile');
-            }, 500);
-        }
+    document.querySelectorAll('.skill-tabs a').forEach(a => {
+      a.classList.remove('tab-active');
     });
+
+    e.target.classList.add('tab-active');
+
+    if (
+      e.target.tagName === 'A' &&
+      document.querySelector(`${priorActive}`) !== targetIcons
+    ) {
+      document.querySelector(`${priorActive}`).classList.remove('visible-tile');
+      setTimeout(() => {
+        targetIcons.classList.add('visible-tile');
+      }, 500);
+    }
+  });
+
+  document.querySelectorAll('.project-tabs').forEach(tabs => {
+    tabs.addEventListener('click', e => {
+      const targetTile = document.querySelector(e.target.dataset.target);
+      const priorActive = document.querySelector('.tab-active').dataset.target;
+
+      document.querySelectorAll('.project-tabs a').forEach(a => {
+        a.classList.remove('tab-active');
+      });
+
+      e.target.classList.add('tab-active');
+
+      document.querySelector(`${priorActive}`).classList.remove('visible-tile');
+
+      targetTile.classList.add('visible-tile');
+    });
+  });
 });
