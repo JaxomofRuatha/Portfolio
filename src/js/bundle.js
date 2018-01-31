c6f9b8573ae64c0c9d00b97de9ee810eda4b3a91
John Chritton <jchritton33@gmail.com>
2018-01-30 23:47:27 -0600

    Updated projects, getting tab content functioning

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,5 +1,21 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 document.addEventListener('DOMContentLoaded', () => {
+  const projectIndex = {
+    'live-1': 'Portfolio',
+    'live-2': 'React Quote Machine',
+    'live-3': 'React Forecast',
+    'live-4': 'Express Microservices',
+    'live-5': 'React .md Previewer',
+    'live-6': 'Redux Wikipedia Viewer',
+    'live-7': 'Speller',
+    'wip-1': 'Bmore Around Town',
+    'wip-2': 'Twitch Dashboard',
+    'wip-3': 'Bikeshare Mapper',
+    'wip-4': 'React Recipe Box',
+    'wip-5': 'JavaScript Calculator',
+    'wip-6': 'Freeform Vote'
+  };
+
   document.querySelector('.skills__menu').addEventListener('click', e => {
     const targetIcons = document.querySelector(e.target.dataset.target);
     const priorActive = document.querySelector('.skills__tab--active').dataset
@@ -24,18 +40,63 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  document.querySelectorAll('project-tabs__menu').forEach(tabs => {
+  document.querySelectorAll('.project-view__menu').forEach(tabs => {
     tabs.addEventListener('click', e => {
-      const targetTile = document.querySelector(e.target.dataset.target);
+      const targetGroup = e.target.dataset.target;
 
       document.getElementById('live-menu').checked = false;
       document.getElementById('wip-menu').checked = false;
 
-      document.querySelectorAll('.project-view__tile').forEach(tile => {
-        tile.classList.remove('project-view__tile--active');
-      });
+      // Change the title of the viewer
+      // Test this using whatever the link text is
 
-      targetTile.classList.add('project-view__tile--active');
+      document.querySelector('.project-view__title').innerHTML =
+        projectIndex[targetGroup];
+
+      // Hide previously visible group content
+
+      document
+        .querySelector('.project-view__tile--active')
+        .classList.remove('project-view__tile--active');
+      document
+        .querySelector('.project-view__links--active')
+        .classList.remove('project-view__links--active');
+      document
+        .querySelector('.project-view__tech--active')
+        .classList.remove('project-view__tech--active');
+
+      // Unhide selected group content
+
+      document
+        .querySelectorAll(`[data-group=${targetGroup}]`)
+        .forEach(piece => {
+          switch (piece.tagName) {
+            case 'SECTION':
+              piece.classList.add('project-view__tile--active');
+              break;
+            case 'NAV':
+              piece.classList.add('project-view__links--active');
+              break;
+            case 'FOOTER':
+              piece.classList.add('project-view__tech--active');
+              break;
+          }
+        });
+
+      /*
+
+      1. Remove the checked status from the menu (aka close)
+      2. Change the innerHtml for .project-view__title
+      3. Add active attributes to each project-view__tile, project-view__links, and project-view__tech (and remove them from the prior one)
+
+      */
+      // const targetTile = document.querySelector(e.target.dataset.target);
+      // document.getElementById('live-menu').checked = false;
+      // document.getElementById('wip-menu').checked = false;
+      // document.querySelectorAll('.project-view__tile').forEach(tile => {
+      //   tile.classList.remove('project-view__tile--active');
+      // });
+      // targetTile.classList.add('project-view__tile--active');
     });
   });
 });
